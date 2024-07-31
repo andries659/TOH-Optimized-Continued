4d4d9e775f451999f85c385adfaa44e0ebb1a7d2
Limeau <148590907+Limeau@users.noreply.github.com>
2024-07-30 22:47:26 -0600

    Create Assassin.cs

---
+++ Roles/Neutral/Assassin.cs
@@ -0,0 +1,64 @@
+using static TOHE.Translator;
+using static TOHE.Options;
+
+namespace TOHE.Roles.Neutral;
+
+internal class Assassin : RoleBase
+{
+    //===========================SETUP================================\\
+    private const int Id = 11400;
+    private static readonly HashSet<byte> playerIdList = [];
+    public static bool HasEnabled => playerIdList.Any();
+    
+    public override CustomRoles ThisRoleBase => CustomRoles.Impostor;
+    public override Custom_RoleType ThisRoleType => Custom_RoleType.NeutralKilling;
+    //==================================================================\\
+
+    private static OptionItem AssassinKillCooldown;
+
+    public override void SetupCustomOption()
+    {
+        SetupRoleOptions(Id, TabGroup.CrewmateRoles, CustomRoles.Assassin);
+        AssassinKillCooldown = FloatOptionItem.Create(Id + 2, GeneralOption.KillCooldown, new(5f, 180f, 2.5f), 30f, TabGroup.CrewmateRoles, false)
+            .SetParent(CustomRoleSpawnChances[CustomRoles.Assassin])
+            .SetValueFormat(OptionFormat.Seconds);
+    }
+    public override void Init()
+    {
+        playerIdList.Clear();
+    }
+    public override void Add(byte playerId)
+    {
+        playerIdList.Add(playerId);
+
+        if (!Main.ResetCamPlayerList.Contains(playerId))
+            Main.ResetCamPlayerList.Add(playerId);
+    }
+    public override void SetKillCooldown(byte id) => Main.AllPlayerKillCooldown[id] = AssassinKillCooldown.GetFloat();
+    public override bool CanUseKillButton(PlayerControl pc) => true;
+    public override bool OnCheckMurderAsKiller(PlayerControl killer, PlayerControl target)
+    {
+        if (killer.Is(CustomRoles.Madmate)) return true;
+        if (target.GetCustomRole().IsCrewmate() && !target.Is(CustomRoles.Madmate) && !target.GetCustomRole().IsConverted())
+        {
+            killer.RpcSetCustomRole(CustomRoles.Madmate);
+            killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Madmate), GetString("AssassinImpostorNotify")));
+            //Utils.NotifyRoles(SpecifySeer: killer);
+            Utils.MarkEveryoneDirtySettings();
+        }
+        if (target.GetCustomRole().IsImpostor() && !target.Is(CustomRoles.Madmate) && !target.GetCustomRole().IsConverted())
+        {
+            killer.RpcSetCustomRole(CustomRoles.Admired);
+            killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Adnired), GetString("AssassinCrewmateNotify")));
+            //Utils.NotifyRoles(SpecifySeer: killer);
+            Utils.MarkEveryoneDirtySettings();
+        }
+        if (target.GetCustomRole().IsNeutral() && !target.Is(CustomRoles.Madmate) && !target.GetCustomRole().IsConverted())
+        {
+            killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Adnired), GetString("AssassinNeutralNotify")));
+            //Utils.NotifyRoles(SpecifySeer: killer);
+            Utils.MarkEveryoneDirtySettings();
+        }
+        return true;
+    }
+}
