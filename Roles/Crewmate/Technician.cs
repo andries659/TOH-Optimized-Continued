d29c4362abbcf62adcf668463ca916835670aa8b
Ape <155788660+ApeMV@users.noreply.github.com>
2024-07-31 09:16:07 +0200

    Update Technician.cs

--- Roles/Crewmate/Technician.cs
+++ Roles/Crewmate/Technician.cs
@@ -1,94 +1,94 @@
-//using static TOHE.Options;
-//using static TOHE.Utils;
-//using TOHE.Roles.Neutral;
-//using static TOHE.Translator;
+using static TOHE.Options;
+using static TOHE.Utils;
+using TOHE.Roles.Neutral;
+using static TOHE.Translator;
 
-//namespace TOHE.Roles.Crewmate;
+namespace TOHE.Roles.Crewmate;
 
-//internal class Technician : RoleBase
-//{
-//    //===========================SETUP================================\\
-//    private const int Id = 29600;
-//    private static readonly HashSet<byte> playerIdList = [];
-//    public static bool HasEnabled => playerIdList.Any();
-//    
-//    public override CustomRoles ThisRoleBase => CustomRoles.Crewmate;
-//    public override Custom_RoleType ThisRoleType => Custom_RoleType.CrewmatePower;
-//    //==================================================================\\
-
-//    private static OptionItem SeeAllIDs;
-//    private static OptionItem SeePlayerInteractions;
-//    private static OptionItem NotifyWhenAnyoneVents;
-//    private static OptionItem NotifyWhenAnyoneShapeshifts;
-
-//    public override void SetupCustomOption()
-//    {
-//        SetupRoleOptions(Id, TabGroup.CrewmateRoles, CustomRoles.Technician);
-//        SeeAllIDs = BooleanOptionItem.Create(Id + 2, "TechnicianSeeAllIds", false, TabGroup.CrewmateRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Technician]);
-//        SeePlayerInteractions = BooleanOptionItem.Create(Id + 3, "TechnicianSeePlayerInteractions", false, TabGroup.CrewmateRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Technician]);
-//        NotifyWhenAnyoneVents = BooleanOptionItem.Create(Id + 4, "TechnicianNotifyWhenAnyoneVents", false, TabGroup.CrewmateRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Technician]);
-//        NotifyWhenAnyoneShapeshifts = BooleanOptionItem.Create(Id + 5, "TechnicianNotifyWhenAnyoneShapeshifts", false, TabGroup.CrewmateRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Technician]);
- //   }
+internal class Technician : RoleBase
+{
+    //===========================SETUP================================\\
+    private const int Id = 29600;
+    private static readonly HashSet<byte> playerIdList = [];
+    public static bool HasEnabled => playerIdList.Any();
     
- //   public override void Init()
- //   {
-//        playerIdList.Clear();
- //   }
-  //  public override void Add(byte playerId)
- //   {
- //       playerIdList.Add(playerId);
-//    }
- //   public override string GetMark(PlayerControl seer, PlayerControl seen, bool isForMeeting = false)
- //   {
-//        if (!seer.IsAlive() || (!SeeAllIDs.GetBool() || !seen.IsAlive())) return string.Empty;
-//        
-//        if (Doppelganger.CheckDoppelVictim(seen.PlayerId))
-//            seen = Doppelganger.GetDoppelControl(seen);
-//        {
-//        return ColorString(GetRoleColor(CustomRoles.Technician), $" {seen.Data.PlayerId}");
-//        }
-//        
-//    }
-//    public static bool ActivateGuardAnimation(byte killerId, PlayerControl target, int colorId)
-//    {
-//        foreach (var technicianId in playerIdList.ToArray())
-//        {   
-//            if (SeePlayerInteractions.GetBool())
-//            {
-//                if (SeePlayerInteractions.GetBool())
-//                if (technicianId == killerId) continue;
-//                var technician = Utils.GetPlayerById(technicianId);
-//               if (technician == null) continue;
-//           
-//
-//                technician.RpcGuardAndKill(target, colorId, true);
-//            }
-//        }
-//        return false;
-//    }
-//    public override bool OnCoEnterVentOthers(PlayerPhysics physics, int ventId)
-//    {
-//        if (NotifyWhenAnyoneVents.GetBool())
-//        {
-//            _ = new LateTask(() =>
-//            {
-//            var technician = _Player;
-//           technician.Notify(GetString("TechnicianNotifyVent"));
-//            }, 0.5f, "Technician Notified of vent");
-//            return true;
-//        }
-//        return false;
-//    }  
-//
-//    public override void OnCheckOthersShapeshift()
-//    {
-//        if (NotifyWhenAnyoneShapeshifts.GetBool())
-//        {
-//            _ = new LateTask(() =>
-//            {
-//            var technician = _Player;
-//            technician.Notify(GetString("TechnicianNotifyShapeshift"));
-//            }, 0.5f, "Technician Notified of Shapeshift");
-//        }
-//    }  
-//}
+    public override CustomRoles ThisRoleBase => CustomRoles.Crewmate;
+    public override Custom_RoleType ThisRoleType => Custom_RoleType.CrewmatePower;
+    //==================================================================\\
+
+    private static OptionItem SeeAllIDs;
+    private static OptionItem SeePlayerInteractions;
+    private static OptionItem NotifyWhenAnyoneVents;
+    private static OptionItem NotifyWhenAnyoneShapeshifts;
+
+    public override void SetupCustomOption()
+    {
+        SetupRoleOptions(Id, TabGroup.CrewmateRoles, CustomRoles.Technician);
+        SeeAllIDs = BooleanOptionItem.Create(Id + 2, "TechnicianSeeAllIds", false, TabGroup.CrewmateRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Technician]);
+        SeePlayerInteractions = BooleanOptionItem.Create(Id + 3, "TechnicianSeePlayerInteractions", false, TabGroup.CrewmateRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Technician]);
+        NotifyWhenAnyoneVents = BooleanOptionItem.Create(Id + 4, "TechnicianNotifyWhenAnyoneVents", false, TabGroup.CrewmateRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Technician]);
+        NotifyWhenAnyoneShapeshifts = BooleanOptionItem.Create(Id + 5, "TechnicianNotifyWhenAnyoneShapeshifts", false, TabGroup.CrewmateRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Technician]);
+    }
+    
+    public override void Init()
+    {
+        playerIdList.Clear();
+    }
+    public override void Add(byte playerId)
+    {
+        playerIdList.Add(playerId);
+    }
+    public override string GetMark(PlayerControl seer, PlayerControl seen, bool isForMeeting = false)
+    {
+        if (!seer.IsAlive() || (!SeeAllIDs.GetBool() || !seen.IsAlive())) return string.Empty;
+        
+        if (Doppelganger.CheckDoppelVictim(seen.PlayerId))
+            seen = Doppelganger.GetDoppelControl(seen);
+        {
+        return ColorString(GetRoleColor(CustomRoles.Technician), $" {seen.Data.PlayerId}");
+        }
+        
+    }
+    public static bool ActivateGuardAnimation(byte killerId, PlayerControl target, int colorId)
+    {
+        foreach (var technicianId in playerIdList.ToArray())
+        {   
+            if (SeePlayerInteractions.GetBool())
+            {
+                if (SeePlayerInteractions.GetBool())
+                if (technicianId == killerId) continue;
+                var technician = Utils.GetPlayerById(technicianId);
+                if (technician == null) continue;
+            
+
+                technician.RpcGuardAndKill(target, colorId, true);
+            }
+        }
+        return false;
+    }
+    public override bool OnCoEnterVentOthers(PlayerPhysics physics, int ventId)
+    {
+        if (NotifyWhenAnyoneVents.GetBool())
+        {
+            _ = new LateTask(() =>
+            {
+            var technician = _Player;
+            technician.Notify(GetString("TechnicianNotifyVent"));
+            }, 0.5f, "Technician Notified of vent");
+            return true;
+        }
+        return false;
+    }  
+
+    public override void OnOthersShapeshift()
+    {
+        if (NotifyWhenAnyoneShapeshifts.GetBool())
+        {
+            _ = new LateTask(() =>
+            {
+            var technician = _Player;
+            technician.Notify(GetString("TechnicianNotifyShapeshift"));
+            }, 0.5f, "Technician Notified of Shapeshift");
+        }
+    }  
+}
