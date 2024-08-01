ab76eea5626e31142cec030409d6263d2065b2aa
Ape <155788660+ApeMV@users.noreply.github.com>
2024-08-01 20:01:47 +0200

    Update Hypnotist.cs

--- Roles/Impostor/Hypnotist.cs
+++ Roles/Impostor/Hypnotist.cs
@@ -1,55 +1,52 @@
-//using AmongUs.GameOptions;
-//using TOHE.Roles.Core;
-//using TOHE.Roles.Impostor;
-//using static TOHE.Translator;
-//using static TOHE.Options;
+using AmongUs.GameOptions;
+using TOHE.Roles.Core;
+using TOHE.Roles.Impostor;
+using static TOHE.Translator;
+using static TOHE.Options;
 
-//namespace TOHE.Roles.Impostor;
+namespace TOHE.Roles.Impostor;
 
-//internal class Hypnotist : RoleBase
-//{
+internal class Hypnotist : RoleBase
+{
     //===========================SETUP================================\\
-    //private const int Id = 29700;
-    //public static bool HasEnabled => CustomRoleManager.HasEnabled(CustomRoles.Hypnotist);
-    //public override CustomRoles ThisRolesBase => CustomRoles.Shapeshifter;
-    //public override Custom_RoleType ThisRoleType => Custom_RoleType.ImpostorKilling;
+    private const int Id = 29700;
+    public static bool HasEnabled => CustomRoleManager.HasEnabled(CustomRoles.Hypnotist);
+
+    public override CustomRoles ThisRoleBase => CustomRoles.Shapeshifter;
+    public override Custom_RoleType ThisRoleType => Custom_RoleType.ImpostorKilling;
     //==================================================================\\
 
-    //public static OptionItem HypnotizeCooldown;
-    //public static OptionItem HypnosisDuration;
-    //private static OptionItem AmountOfHypnitizes;
+    private static OptionItem HypnotizeCooldown;
+    private static OptionItem HypnosisDuration;
+    private static OptionItem AmountOfHypnitizes;
+    private static OptionItem ShowShapeshiftAnimationsOpt;
 
-    //public override void SetupCustomOption()
-    //{
-        //Options.SetupRoleOptions(Id, TabGroup.ImpostorRoles, CustomRoles.Hypnotist);
-        //HypnotizeCooldown = FloatOptionItem.Create(Id + 2, "HypnotizeCooldown", new(10f, 30f, 0,5f), 15f, TabGroup.ImpostorRoles, false)
-           //.SetParent(Options.CustomRolesSpawnChance[CustomRoles.Hypnotist])
-           //.SetValueFormat(OptionFormat.Multiplier);
-    //    HypnosisDuration = FloatOptionItem.Create(Id + 3, "HypnosisDuration", new(5f, 25f, 0,5f), 10f, TabGroup.ImpostorRoles, false)
-    //       .SetValueFormat(OptionFormat.Seconds);
-    //    AmountOfHypnitizes = IntegerOptionItem.Create(Id + 10, "HypnotistMaxCount", new(1, 7, 1), 3, TabGroup.ImpostorRoles, false)
-    //        .SetParent(Options.CustomRoleSpawnChances[CustomRoles.Hypnotist])
-    //        .SetValueFormat(OptionFormat.Times);
-     //   ShowShapeshiftAnimationsOpt = BooleanOptionItem.Create(Id + 4, GeneralOption.ShowShapeshiftAnimations, true, TabGroup.ImpostorRoles, false)
-     //       .SetParent(Options.CustomRoleSpawnChances[CustomRoles.Hypnotist]);
-  //  }
-    //public override void Init()
-//    {
-    //    ForHypnotist.Clear();
-  //  }
-  //  }
-   // public override void Init()
-  //  {
-   //     playerIdList.clear();
-   // }
-   // public override void Add(byte playerId)
- //   {
-  //      playerIdList.Add(playerId);
-  //  }
-  //  public override bool IsEnable => playerIdList.Any();
+    private static readonly HashSet<byte> playerIdList = [];
 
-  //  public override void ApplyGameOptions(IgameOptions opt, byte playerId)
-  //  {
-  //      AURoleOptions.ShapeshifterCooldown = HypnotizeCooldown.GetFloat();
-   //     AURoleOptions.ShapeshifterDuration = 1f;
-   // }
+    public override void SetupCustomOption()
+    {
+        SetupRoleOptions(Id, TabGroup.ImpostorRoles, CustomRoles.Hypnotist);
+        HypnotizeCooldown = FloatOptionItem.Create(Id + 2, "HypnotistHypnotizeCooldown", new(2.5f, 180f, 2.5f), 15f, TabGroup.ImpostorRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Hypnotist])
+           .SetValueFormat(OptionFormat.Seconds);
+        HypnosisDuration = FloatOptionItem.Create(Id + 3, "HypnotistHypnosisDuration", new(2.5f, 180f, 1.5f), 10f, TabGroup.ImpostorRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Hypnotist])
+           .SetValueFormat(OptionFormat.Seconds);
+        AmountOfHypnitizes = IntegerOptionItem.Create(Id + 4, "HypnotistMaxCount", new(1, 7, 1), 3, TabGroup.ImpostorRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Hypnotist])
+            .SetValueFormat(OptionFormat.Times);
+        ShowShapeshiftAnimationsOpt = BooleanOptionItem.Create(Id + 5, GeneralOption.ShowShapeshiftAnimations, true, TabGroup.ImpostorRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Hypnotist]);
+    }
+    public override void Init()
+    {
+        playerIdList.Clear();
+    }
+    public override void Add(byte playerId)
+    {
+        playerIdList.Add(playerId);
+    }
+    public override bool IsEnable => playerIdList.Any();
+
+    public override void ApplyGameOptions(IGameOptions opt, byte playerId)
+    {
+        AURoleOptions.ShapeshifterCooldown = HypnotizeCooldown.GetFloat();
+        AURoleOptions.ShapeshifterDuration = 1f;
+    }
+}
