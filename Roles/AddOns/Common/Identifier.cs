e5e069e645a46948717406d72fd4d657ef758902
Ape <155788660+ApeMV@users.noreply.github.com>
2024-08-01 15:53:01 +0200

    Add Identifier (WIP)

---
+++ Roles/AddOns/Common/Identifier.cs
@@ -0,0 +1,34 @@
+﻿namespace TOHE.Roles.AddOns.Common;
+
+public static class Identifier
+{
+    private const int Id = 30100;
+
+    public static OptionItem ImpCanBeIdentifier;
+    public static OptionItem CrewCanBeIdentifier;
+    public static OptionItem NeutralCanBeIdentifier;
+    
+    public static Dictionary<byte, string> IdentifierNotify = [];
+
+    public static void SetupCustomOptions()
+    {
+        Options.SetupAdtRoleOptions(Id, CustomRoles.Identifier, canSetNum: true);
+        ImpCanBeIdentifier = BooleanOptionItem.Create(Id + 10, "ImpCanBeIdentifier", true, TabGroup.Addons, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Identifier]);
+        CrewCanBeIdentifier = BooleanOptionItem.Create(Id + 11, "CrewCanBeIdentifier", true, TabGroup.Addons, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Identifier]);
+        NeutralCanBeIdentifier = BooleanOptionItem.Create(Id + 12, "NeutralCanBeIdentifier", true, TabGroup.Addons, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Identifier]);
+    }
+
+    public static void Init()
+    {
+        IdentifierNotify = [];
+    }
+    public static void Clear()
+    {
+        IdentifierNotify.Clear();
+    }
+
+    public static void OnReportDeadBody(PlayerControl reporter, NetworkedPlayerInfo deadBody)
+    {
+
+    }
+}
