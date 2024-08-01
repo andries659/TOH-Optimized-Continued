9085c48bf5160677f0d37bca89bd35373a791ea1
Ape <155788660+ApeMV@users.noreply.github.com>
2024-08-01 11:18:20 +0200

    Update Underclocked.cs

--- Roles/AddOns/Common/Underclocked.cs
+++ Roles/AddOns/Common/Underclocked.cs
@@ -10,7 +10,8 @@ public static class Underclocked
 
     public static void SetupCustomOptions()
     {
+        SetupAdtRoleOptions(Id, CustomRoles.Underclocked, canSetNum: true);
         UnderclockedIncrease = FloatOptionItem.Create(Id + 10, "UnderclockedIncrease", new(5f, 180f, 2.5f), 15f, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Underclocked])
-            .SetValueFormat(OptionFormat.Seconds);
+            .SetValueFormat(OptionFormat.Percent);
     }
 }
