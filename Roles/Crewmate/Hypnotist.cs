c62c7c617eff622b2cc23e0f0e26b0bd85ffe3d1
AndriesWorks <168019743+andries659@users.noreply.github.com>
2024-09-28 21:24:06 +0200

    Update Hypnotist.cs

--- Roles/Crewmate/Hypnotist.cs
+++ Roles/Crewmate/Hypnotist.cs
@@ -1,4 +1,15 @@
 using AmongUs.GameOptions
 using Hazel 
 
-namespace 
+namespace TOHE.Roles.Impostor;
+
+internal class Hypnotist : RoleBase
+{
+    //===========================SETUP================================\\
+    private const int Id = 30200;
+    private static readonly HashSet<byte> playerIdList = [];
+    public static bool HasEnabled => playerIdList.Any();
+    
+    public override CustomRoles ThisRoleBase => CustomRoles.Impostor;
+    public override Custom_RoleType ThisRoleType => Custom_RoleType.CrewmateBasic;
+    //==================================================================\\
