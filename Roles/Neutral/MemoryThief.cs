93fbbe9d4363c377c30cd1d3ae41c702beaf5d87
AndriesWorks <168019743+andries659@users.noreply.github.com>
2024-09-29 16:23:36 +0200

    Update MemoryThief.cs

--- Roles/Neutral/MemoryThief.cs
+++ Roles/Neutral/MemoryThief.cs
@@ -5,7 +5,7 @@ namespace TOHE.Roles.Neutral;
 internal class MemoryThief : RoleBase
 {
     //===========================SETUP================================\\
-    private const int Id = 30210;
+    private const int Id = 30300;
     public override CustomRoles ThisRoleBase => CustomRoles.Neutral;
     public override Custom_RoleType ThisRoleType => Custom_RoleType.NeutralChaos;
     //==================================================================\\
