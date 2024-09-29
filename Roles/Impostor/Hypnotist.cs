4ba6843d921a40ca2cc169182460ee577531898f
AndriesWorks <168019743+andries659@users.noreply.github.com>
2024-09-29 08:55:02 +0200

    Update Hypnotist.cs

--- Roles/Impostor/Hypnotist.cs
+++ Roles/Impostor/Hypnotist.cs
@@ -1,3 +1,4 @@
+
 using AmongUs.GameOptions;
 using Hazel;
 
@@ -17,9 +18,9 @@ internal class Hypnotist : RoleBase
     public override void SetupCustomOption()
         {
         Options.SetupRoleOptions(Id, TabGroup.ImpostorRoles, CustomRoles.Hypnotist);
-        AbilityCooldown = FloatOptionItem.Create(Id + 2, GeneralOption.ShapeshifterBase_ShapeshiftCooldown, new(1f, 180f, 1f), 25f, TabGroup.ImpostorRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Hypnotist])
+        AbilityCooldown = FloatOptionItem.Create(Id + 2, "AbilityCooldown", new(1f, 180f, 1f), 25f, TabGroup.ImpostorRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Hypnotist])
             .SetValueFormat(OptionFormat.Seconds);
-        AbilityDuration = FloatOptionItem.Create(Id + 4, GeneralOption.ShapeshifterBase_ShapeshiftDuration, new(1f, 180f, 1f), 25f, TabGroup.ImpostorRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Hypnotist])
+        AbilityDuration = FloatOptionItem.Create(Id + 4, "AbilityDuration", new(1f, 180f, 1f), 25f, TabGroup.ImpostorRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Hypnotist])
             .SetValueFormat(OptionFormat.Seconds);
     }
 }
